3c3f6a6bf51b7c6e607bd0519dd636cd484d95bb
Ken Duigraha Putra <email": null>
2016-11-13 00:21:16 +0700

    added gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+const gulp = require('gulp')
+const gulpStylus = require('gulp-stylus')
+const browserSync = require('browser-sync')
+
+gulp.task("css", function() {
+  return gulp.src('./resource/resource.styl')
+    .pipe(gulpStylus())
+    .pipe(gulp.dest('./style/'));
+})
+
+gulp.task("run", function(){
+  browserSync.init({
+      server: {
+          baseDir: "./"
+      }
+  })
+
+  gulp.watch("./resource/*.styl", ['css'])
+  gulp.watch("./style/*.css").on("change", browserSync.reload)
+  gulp.watch("./*.html").on("change", browserSync.reload)
+})
